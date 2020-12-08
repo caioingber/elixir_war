b23da9a314103b557fbb8ad757479c88ad4c362e
caio_ingber <caioingber@gmail.com>
2020-12-08 00:18:33 -0600

    comparison

--- lib/elixir_war.ex
+++ lib/elixir_war.ex
@@ -41,26 +41,55 @@ defmodule ElixirWar do
     def ranks, do: @ranks
     def score, do: @score
 
+    @spec build_deck() :: [map()]
     def build_deck do
-      scores = score()
+      score = score()
       ranks = ranks()
       suits = suits()
 
-      Enum.map(ranks, fn rank ->
-        index = Enum.find_index(ranks, fn value -> value == rank end)
-        {_, card_score} = Enum.fetch(scores, index)
-        score_with_rank = %{score: card_score, rank: rank}
-        Enum.map(suits, fn suit ->
-          card(score_with_rank, suit)
-        end)
-      end)
+      build_cards(ranks, suits, score)
       |> List.flatten()
       |> Enum.shuffle()
     end
 
+    @spec card(map(), String.t()) :: map()
     def card(%{score: score, rank: rank}, suit) do
       %{rank: rank, score: score, suit: suit, description: "#{rank} of #{suit}"}
     end
-  end
 
+    @spec build_cards(list(), list(), list()) :: [map()]
+    def build_cards(ranks, suits, score) do
+      Enum.map(ranks, fn rank ->
+        index = Enum.find_index(ranks, fn value -> value == rank end)
+        {_, card_score} = Enum.fetch(score, index)
+        score_with_rank = %{score: card_score, rank: rank}
+
+        Enum.map(suits, fn suit ->
+          card(score_with_rank, suit)
+        end)
+      end)
+    end
+
+    @spec deal([map()]) :: {list(), list()}
+    def deal(deck) do
+      {player_one, player_two} = Enum.split(deck, floor(length(deck) / 2))
+      [player_one, player_two]
+    end
+
+
+    @spec play_round([list()]) :: any()
+    def play_round([player_one, player_two] = hands) do
+      [{card_one, _hand_one}, {card_two, _hand_two}] = Enum.map(hands, fn hand ->
+        List.pop_at(hand, 0)
+      end)
+
+      compare_cards(card_one, card_two)
+    end
+
+    def compare_cards(%{score: score_one}, %{score: score_two}) when score_one > score_two, do: "Player One"
+    def compare_cards(%{score: score_one}, %{score: score_two}) when score_one < score_two, do: "Player Two"
+    def compare_cards(_, _), do: "War!"
+
+
+  end
 end
